47b46c07354245d4ab3d8b12bc4a72ffc9c82695
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-04-28 11:05:52 +0000

    Ajout d'un nouveau fichier TypeScript pour définir les types et l'état du panier : création des interfaces Cart et CartState, incluant des méthodes pour gérer les opérations sur le panier et le processus de paiement. Cela améliore la structure et la typage des données liées au panier.

---
+++ src/types/cart.ts
@@ -0,0 +1,26 @@
+import { CartItem } from './cartItem';
+import { BillingAddress } from '@/services/cartService';
+import { CustomerInfo } from '@/store/cartStore';
+
+export interface Cart {
+  items: CartItem[];
+  total: number;
+}
+
+export interface CartState extends Cart {
+  loading: boolean;
+  error: string | null;
+  checkoutUrl: string | null;
+  sessionId: string | null;
+  fetchCart: () => Promise<void>;
+  addToCart: (courseId: number) => Promise<void>;
+  removeFromCart: (courseId: number) => Promise<void>;
+  clearCart: () => Promise<void>;
+  isInCart: (courseId: number) => boolean;
+  checkout: (
+    successUrl: string, 
+    cancelUrl: string, 
+    billingAddress: BillingAddress,
+    customerInfo: CustomerInfo
+  ) => Promise<string | null>;
+} 
